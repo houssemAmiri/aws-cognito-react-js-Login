b3582acecad48f61f5e89158c478491a6c48dcd9
Amiri Houssem <houssem.amiri2@gmail.com>
2021-03-31 21:50:28 +0100

    testing aws amplify

--- src/App.js
+++ src/App.js
@@ -1,25 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import { withAuthenticator, AmplifySignOut } from "@aws-amplify/ui-react";
+
+import "./App.css";
+import Amplify, { Auth } from "aws-amplify";
+import awsconfig from "./aws-exports";
+Amplify.configure(awsconfig);
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <AmplifySignOut />
+        My App
       </header>
     </div>
   );
 }
 
-export default App;
+export default withAuthenticator(App);
