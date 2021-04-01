694a5ca25281062d8ff18d9b107a744de03fe6d5
Amiri Houssem <houssem.amiri2@gmail.com>
2021-04-01 22:05:26 +0100

    adding amplify

--- src/index.js
+++ src/index.js
@@ -6,6 +6,7 @@ import reportWebVitals from "./reportWebVitals";
 import Amplify from "aws-amplify";
 import awsconfig from "./aws-exports";
 Amplify.configure(awsconfig);
+
 ReactDOM.render(
   <React.StrictMode>
     <App />
